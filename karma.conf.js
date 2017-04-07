c4288608379a85a4578a99908c4abd3d0e8eba93
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-04-07 18:06:16 +0200

    update reporters

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['spec', 'failed', 'coverage'],
 
 
     // web server port
